68fc059dd4f19b01ed3087ebc63dc755a53652f5
Fakorede Oluwaleke <hashilekky@gmail.com>
2023-08-10 12:40:43 +0100

    Implemented unit and e2e testing

---
+++ src/test/data/user.ts
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+
+export const userData = {
+  email: faker.internet.email(),
+  deviceId: faker.string.uuid(),
+  fullName: faker.internet.displayName(),
+  password: 'Janetdoesnotloveyou234$',
+  otp: faker.string.numeric(6),
+  signUpAuthToken: faker.string.alphanumeric(32),
+  authToken: faker.string.alphanumeric(32),
+};
