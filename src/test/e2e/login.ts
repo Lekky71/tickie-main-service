200116286c037a827311369cf346507927219509
myke <awoniranopeyemi@gmail.com>
2023-08-17 13:37:07 +0100

    udpate

--- src/test/e2e/login.ts
+++ src/test/e2e/login.ts
@@ -7,8 +7,6 @@ import { connectDBForTesting } from '../connect.db.for.testing';
 import randomstring from 'randomstring';
 import { JwtHelper } from '../../helpers/jwt.helper';
 import { userData } from '../data/user';
-import { BadRequestError } from '../../interfaces';
-
 
 
 export const LoginE2E = () => {
@@ -20,57 +18,70 @@ export const LoginE2E = () => {
     await connectDBForTesting();
   });
 
-    it('login in a user with valid email and password', function (done) {
-        const loginDetails = {
-            email: userData.email,
-            password: userData.password
-        };
-        const deviceId = userData.deviceId;
+  it('logs in a user with valid email and password', function (done) {
+    const loginDetails = {
+      email: userData.email,
+      password: userData.password
+    };
+    const deviceId = userData.deviceId;
+
+    JwtHelper.prototype.generateToken = jest.fn().mockReturnValueOnce(userData.authToken);
     request(app)
       .post('/user/auth/login')
       .set('x-device-id', deviceId)
       .send(loginDetails)
       .expect(200, (err, res) => {
-          if (err) return done(err);
-          expect(res.body).toEqual({
-            token: userData.authToken,
-            user: userData
-        });
+        if (err) {
+          return done(err)
+        }
+        expect(res.body).toHaveProperty('token', userData.authToken);
+        expect(res.body).toHaveProperty('user');
+        expect(res.body.user).toHaveProperty('email', userData.email.toLowerCase());
+        expect(res.body.user).toHaveProperty('fullName', userData.fullName);
         done();
       });
   });
 
-    it('throws BadRequestError for incorrect password', async (done) => {
-       const loginDetails = {
-            email: userData.email,
-            password: 'wrongPassword'// simulating wrong password
-       };
-        const deviceId = userData.deviceId;
-    request(app)
-      .post('/user/auth/login')
-      .set('x-device-id', deviceId)
-      .send(loginDetails)
-      .expect(200, (err, res) => {
-          if (err) { return done(err); }
+  it('throws BadRequestError for incorrect password', async (done) => {
+    const loginDetails = {
+      email: userData.email,
+      password: 'wrongPassword'// simulating wrong password
+    };
 
-        expect(res.body).rejects.toThrow(BadRequestError);
-        done();
-      });
-  });
-
-    it('throws BadRequestError for unrecognized device', async (done) => {
-      const loginDetails = {
-            email: userData.email,
-            password: userData.password
-      };
-    const deviceId= 'lslsiiss';// giving it wrong user device Id
+    const expectedError = {
+      "error": true,
+      "message": "Invalid login details"
+    }
+    const deviceId = userData.deviceId;
     request(app)
       .post('/user/auth/login')
       .set('x-device-id', deviceId)
       .send(loginDetails)
       .expect(400, (err, res) => {
-          if (err) { return done(err); }
-         expect(res.body).rejects.toThrow(BadRequestError);
+        if (err) {
+          return done(err);
+        }
+
+        expect(res.body).toHaveProperty('message', 'Invalid login details');
+        done();
+      });
+  });
+
+  it('throws BadRequestError for unrecognized device', async (done) => {
+    const loginDetails = {
+      email: userData.email,
+      password: userData.password
+    };
+    const deviceId = 'lslsiiss';// giving it wrong user device Id
+    request(app)
+      .post('/user/auth/login')
+      .set('x-device-id', deviceId)
+      .send(loginDetails)
+      .expect(400, (err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res.body).toHaveProperty('message', 'Device not recognised, enter OTP sent to mail to verify');
         done();
       });
   });
